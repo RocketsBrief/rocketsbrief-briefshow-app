3777769b46753172621d2b3db9dc5450209f172f
RocketsBrief <desk@rocketsbrief.com>
2026-07-06 14:00:10 +0200

    Initial Commit

---
+++ BriefShow/BriefShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BriefShowApp.swift
+//  BriefShow
+//
+//  Created by Esti Wahyuni on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct BriefShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BriefShow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BriefShow
+//
+//  Created by Esti Wahyuni on 7/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
